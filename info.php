9eac30e428c5814c23a81b9da5b27b2134ad741e
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-02 17:09:53 +0530

    interface updated

--- info.php
+++ info.php
@@ -4,7 +4,7 @@
   $abc1="SELECT elecname,teachernm,seats,link FROM elective WHERE 1 ";
   if($result1 = mysqli_query($connection, $abc1))
   {
-    echo '<table class="table" style="width: 70%;"><thead><tr>';
+    echo '<table class="table"><thead><tr>';
     echo '<th>electives</th><th>teacher</th><th>seats</th><th>syllabus link</th></tr></thead><tbody><tr>';
   	while($rowa = mysqli_fetch_array($result1))
   	{
@@ -12,11 +12,11 @@
   		echo "<td>".$rowa['elecname']."</td><td>".$rowa['teachernm']."</td><td> ".$rowa['seats']."</td><td> ".$rowa['link']."</td></tr>";
 
   	}
-    echo '</tbody></table>';
+    echo '</tbody></table><br>';
     
   }
   echo "<center>Published Electives</center>";
-  echo '<table class="table" style="width: 70%;"><thead><tr>';
+  echo '<table class="table"><thead><tr>';
   $abc2="SELECT elecname,teachernm,seats,link FROM elective WHERE publish=1";
   echo '<th>electives</th><th>teacher</th><th>seats</th><th>syllabus link</th></tr></thead><tbody><tr><td>';
     if($result2 = mysqli_query($connection, $abc2))
