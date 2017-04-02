63188119d1aa5732b0b130d6429a692c8c7c0bf7
Rishabh04-02 <rishabh0402@gmail.com>
2017-04-02 18:27:48 +0530

    syllabus updated

--- student/student_syllabus.php
+++ student/student_syllabus.php
@@ -2,66 +2,12 @@
 
 <div class="inbox-body">
                          <div class="mail-option">
-                             <div class="chk-all">
-                                 <input type="checkbox" class="mail-checkbox mail-group-checkbox">
-                                 <div class="btn-group">
-                                     <a data-toggle="dropdown" href="#" class="btn mini all" aria-expanded="false">
-                                         All
-                                         <i class="fa fa-angle-down "></i>
-                                     </a>
-                                     <ul class="dropdown-menu">
-                                         <li><a href="#"> None</a></li>
-                                         <li><a href="#"> Read</a></li>
-                                         <li><a href="#"> Unread</a></li>
-                                     </ul>
-                                 </div>
-                             </div>
-
-                             <div class="btn-group">
-                                 <a data-original-title="Refresh" data-placement="top" data-toggle="dropdown" href="#" class="btn mini tooltips">
-                                     <i class=" fa fa-refresh"></i>
-                                 </a>
-                             </div>
-                             <div class="btn-group hidden-phone">
-                                 <a data-toggle="dropdown" href="#" class="btn mini blue" aria-expanded="false">
-                                     More
-                                     <i class="fa fa-angle-down "></i>
-                                 </a>
-                                 <ul class="dropdown-menu">
-                                     <li><a href="#"><i class="fa fa-pencil"></i> Mark as Read</a></li>
-                                     <li><a href="#"><i class="fa fa-ban"></i> Spam</a></li>
-                                     <li class="divider"></li>
-                                     <li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
-                                 </ul>
-                             </div>
-                             <div class="btn-group">
-                                 <a data-toggle="dropdown" href="#" class="btn mini blue">
-                                     Move to
-                                     <i class="fa fa-angle-down "></i>
-                                 </a>
-                                 <ul class="dropdown-menu">
-                                     <li><a href="#"><i class="fa fa-pencil"></i> Mark as Read</a></li>
-                                     <li><a href="#"><i class="fa fa-ban"></i> Spam</a></li>
-                                     <li class="divider"></li>
-                                     <li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
-                                 </ul>
-                             </div>
-
-                             <ul class="unstyled inbox-pagination">
-                                 <li><span>1-50 of 234</span></li>
-                                 <li>
-                                     <a class="np-btn" href="#"><i class="fa fa-angle-left  pagination-left"></i></a>
-                                 </li>
-                                 <li>
-                                     <a class="np-btn" href="#"><i class="fa fa-angle-right pagination-right"></i></a>
-                                 </li>
-                             </ul>
-                         </div>
+                             
                           <table class="table table-inbox table-hover">
                             <tbody>
                               <tr class="unread">
-                                  <td class="view-message  dont-show">Roll No.</td>
-                                  <td class="view-message ">Department</td>
+                                  <td class="view-message  dont-show">Total Seats</td>
+                                  <td class="view-message ">Elective - syllabus</td>
                           
                               </tr>
 
@@ -89,7 +35,7 @@
                               <tr class="unread">
                                   
                                   <td class="view-message "><?php echo $seats; ?></td>
-                                  <td class="view-message "><?php echo $link; ?></td>
+                                  <td class="view-message "><a href="<?php echo $link; ?>" target="_blank"><?php echo $link; ?></a></td>
                                   
                                               
 
