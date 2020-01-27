89726c02e45973fcc1c6404862c1a1f314a509b5
Rishabh <Rishabh04-02@users.noreply.github.com>
2020-01-27 23:47:57 +0530

    Update and rename dbconnect.php~ to sample-dbconnect.php

--- sample-dbconnect.php
+++ sample-dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 # $ php -f db-connect-test.php
 $dbname = 'openelectivems';
-$dbuser = 'root';
-$dbpass = 'hey';
-$dbhost = 'localhost';
+$dbuser = 'USERNAME';
+$dbpass = 'PASSWORD';
+$dbhost = 'HOST';
 
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysqli_select_db($connection, $dbname) or die("Could not open the db '$dbname'");
